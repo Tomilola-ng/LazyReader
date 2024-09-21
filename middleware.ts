58737ff513239cfaf2bd893ce808655b65237fc5
BeautifulAngel246 <favouronuche@gmail.com>
2024-09-21 06:34:08 -0700

    done

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,22 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server"; 
+import { clerkMiddleware } from "@clerk/nextjs/server";
 import { _api } from "./types/api";
 
 export async function middleware(req: NextRequest) {
   const accessToken = req.cookies.get("access_cookie")?.value;
 
-  const authResponse: _api["getAuth"] = () => {
-    // Run your authentication logic here
-    return {
-      status: 200,
-      body: {
-        accessToken: accessToken,
-      },
-    };
-  };
-
-  if (authResponse()) {
-    return NextResponse.next();
+  // Check if accessToken exists to determine authentication
+  if (accessToken) {
+    return NextResponse.next(); // Proceed if authenticated
   } else {
-    return NextResponse.redirect(new URL("/login?NotLoggedIn=True", req.url));
+    return NextResponse.redirect(new URL("/login?NotLoggedIn=True", req.url)); // Redirect if not authenticated
   }
 }
 
+// Apply Clerk middleware
+export default clerkMiddleware();
+
+// Configure the routes the middleware applies to
 export const config = {
   matcher: [
     "/admin",
@@ -28,5 +24,10 @@ export const config = {
     "/dashboard/:path*",
     "/super-admin",
     "/super-admin/:path*",
+    // Skip Next.js internals and all static files
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
 };
+

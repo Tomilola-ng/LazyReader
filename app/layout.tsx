83f0cba1513b7a8bf4407ddde570510d14f2ab42
BeautifulAngel246 <favouronuche@gmail.com>
2024-09-22 18:19:21 -0700

    webmanifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,31 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ClerkProvider, SignedIn, SignedOut, UserButton, SignInButton } from '@clerk/nextjs';
+
 import "./globals.css";
+
 import { _siteDetails } from "@/lib/config";
 import { Providers } from "./providers";
 
-// Import the Inter font with specific weights
 const myFont = Inter({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800", "900"],
 });
 
-// Set the metadata for the page
 export const metadata: Metadata = {
   title: _siteDetails.name,
   description: _siteDetails.desc,
 };
 
-// Define the RootLayout component
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${myFont.className} hide-scrollbar`}>
-        <ClerkProvider>
-          <Providers>{children}</Providers>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-        </ClerkProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
 }
-
--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,26 @@
-import { NextRequest, NextResponse } from "next/server"; 
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
 import { _api } from "./types/api";
 
 export async function middleware(req: NextRequest) {
   const accessToken = req.cookies.get("access_cookie")?.value;
 
-  // Check if accessToken exists to determine authentication
-  if (accessToken) {
-    return NextResponse.next(); // Proceed if authenticated
+  const authResponse: _api["getAuth"] = () => {
+    // Run your authentication logic here
+    return {
+      status: 200,
+      body: {
+        accessToken: accessToken,
+      },
+    };
+  };
+
+  if (authResponse()) {
+    return NextResponse.next();
   } else {
-    return NextResponse.redirect(new URL("/login?NotLoggedIn=True", req.url)); // Redirect if not authenticated
+    return NextResponse.redirect(new URL("/login?NotLoggedIn=True", req.url));
   }
 }
 
-// Apply Clerk middleware
-export default clerkMiddleware();
-
-// Configure the routes the middleware applies to
 export const config = {
   matcher: [
     "/admin",
@@ -24,10 +28,5 @@ export const config = {
     "/dashboard/:path*",
     "/super-admin",
     "/super-admin/:path*",
-    // Skip Next.js internals and all static files
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
   ],
 };
-

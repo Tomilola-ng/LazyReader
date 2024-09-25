a1135cc0799d48797ba02fdcbe5f06fbdd481a77
tee.o2809 <tee.o2809@gmail.com>
2024-09-25 09:27:57 +0000

    Failed results due to Gemini required file uri input ype

--- app/api/summarize/route.ts
+++ app/api/summarize/route.ts
@@ -1,59 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import { GoogleAIFileManager } from "@google/generative-ai/server";
-import fs from "fs";
-import path from "path";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
-const fileManager = new GoogleAIFileManager(
-  process.env.GEMINI_API_KEY as string
-);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
 
 export async function POST(req: NextRequest) {
+  
   try {
-    const contentType = req.headers.get("content-type") || "";
-    if (!contentType.includes("multipart/form-data")) {
-      return NextResponse.json(
-        { message: "Content-Type must be multipart/form-data" },
-        { status: 400 }
-      );
-    }
-
-    const formData = await req.formData();
-    const file = formData.get("file") as File;
+    const { file } = await req.json();
 
     if (!file) {
-      return NextResponse.json({
-        message: "No file was uploaded.",
-        status: 400,
-      });
-    }
-
-    const filePath = path.join(process.cwd(), "tmp", file.name);
-
-    fs.mkdirSync(path.dirname(filePath), { recursive: true });
-
-    const buffer = Buffer.from(await file.arrayBuffer());
-    fs.writeFileSync(filePath, buffer);
-
-    const uploadResponse = await fileManager.uploadFile(filePath, {
-      mimeType: file.type,
-      displayName: file.name,
-    });
-
-    if (!uploadResponse.file) {
-      return NextResponse.json({
-        message: "Failed to upload file.",
-        status: 500,
-      });
+      return NextResponse.json(
+        { message: "No file URL provided." },
+        { status: 400 }
+      );
     }
 
     const result = await model.generateContent([
       {
         fileData: {
-          mimeType: uploadResponse.file.mimeType,
-          fileUri: uploadResponse.file.uri,
+          mimeType: "application/pdf",
+          fileUri: file,
         },
       },
       { text: "Can you give a detailed summary of this ebook?" },
@@ -67,9 +34,6 @@ export async function POST(req: NextRequest) {
         status: 500,
       });
     }
-
-    // Remove the file after use
-    // fs.unlinkSync(filePath);
 
     return NextResponse.json({
       message: "File summarized successfully.",
